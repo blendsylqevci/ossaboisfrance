74f7a8446c500d46fda6e37d27d2aee920b76914
Blend Sylqevci <sylqevciblendi@gmail.com>
2026-05-25 00:13:48 +0200

    fix: bypass postgres ssl unauthorized cert in production

--- payload.config.ts
+++ payload.config.ts
@@ -53,6 +53,7 @@ export default buildConfig({
       connectionString: process.env.DATABASE_URI || '',
       max: 10,
       idleTimeoutMillis: 1000,
+      ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
     },
     push: false,
   }),
